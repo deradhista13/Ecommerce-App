e42e0f8c8b6d59793379a590cd774ac69974a19a
deradhista <deradhista@gmail.com>
2023-01-24 15:31:38 +0700

    Add: Components Hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div
+      className="hero h-72 rounded-lg"
+      style={{
+        backgroundImage: `url("https://i.pinimg.com/564x/a6/fe/63/a6fe63a164987af4143a8ecc0a592955.jpg")`,
+      }}
+    ></div>
+  );
+};
+
+export default Hero;
