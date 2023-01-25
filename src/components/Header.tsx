3922e740ed1b063fb120d74766777c4da4951ad9
deradhista <deradhista@gmail.com>
2023-01-25 10:54:00 +0700

    feat: fixing routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,50 @@
 import React from "react";
 import logo from "../assets/logo.svg";
 import avatar from "../assets/avatar.svg";
-import { FiRepeat, FiShoppingBag, FiShoppingCart, FiSearch } from "react-icons/fi";
+import {
+  FiRepeat,
+  FiShoppingBag,
+  FiShoppingCart,
+  FiSearch,
+} from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <header className="header sticky top-0 bg-white shadow-md flex items-center justify-between px-28 py-2">
+      <div className="navbar w-full sticky top-0 bg-white shadow-md flex items-center justify-between px-28 py-2">
         <h1 className="w-fit">
-          <a href="">
+          <Link to="/">
             <img src={logo} width="60px" alt="logo-nav" />
-          </a>
+          </Link>
         </h1>
 
         <div className="flex bg-gray-100 p-2 w-96 space-x-4 rounded-full items-center justify-between">
-          <input className="bg-gray-100 outline-none" type="text" placeholder="Search" />
+          <input
+            className="bg-gray-100 outline-none"
+            type="text"
+            placeholder="Search"
+          />
           <a href="">
             <FiSearch size={20} />
           </a>
         </div>
 
         <div className="w-fit flex justify-end items-center gap-10">
-          <a href="">
+          <Link to="/transaksi_beli">
             <FiRepeat size={20} />
-          </a>
-          <a href="">
+          </Link>
+          <Link to="/detail_product">
             <FiShoppingBag size={20} />
-          </a>
-          <a href="">
+          </Link>
+          <Link to="/cart">
             <FiShoppingCart size={20} />
-          </a>
+          </Link>
           <a href="">
             <img src={avatar} className="rounded-full" width="30px" alt="" />
           </a>
         </div>
-      </header>
+      </div>
     </div>
   );
 };
